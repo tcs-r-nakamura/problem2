28ba375b2e98943dad18a138f659ecb3ca8f7c34
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-07 09:20:10 +0900

    メソッド分割

--- problem2/Program.cs
+++ problem2/Program.cs
@@ -3,12 +3,23 @@ class Program2
 {
     static void Main(string[] args)
     {
-        int.TryParse(args[0], out var year);
+        if (!int.TryParse(args[0], out var year))
+        {
+            Console.WriteLine("整数の年を入力してください");
+            return;
+        }
         if (year < 0)
         {
             Console.WriteLine("正数を入力してください");
-        }//もし4で割り切れかつ、100で割り切れない、または400で割り切れる
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            return;
+        }
+        ShowYear(year);
+
+    }
+    //うるう年表示メソッド
+    static void ShowYear(int year)
+    {
+        if (IsLeapYear(year))
         {
             Console.WriteLine("うるう年です");
         }
@@ -17,5 +28,18 @@ class Program2
             Console.WriteLine("うるう年ではありません");
         }
     }
-}
+    //うるう年判定メソッド
+    static bool IsLeapYear(int year)
+    {
+        //もし4で割り切れかつ、100で割り切れない、または400で割り切れる
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
+}
